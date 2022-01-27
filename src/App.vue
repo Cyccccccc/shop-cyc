fcfae40d6fa4d5d85a5f84c6ed72b2e1f9f63770
Cyc <695805868@qq.com>
2022-01-27 20:23:42 +0800

    day01

---
+++ src/App.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <Header></Header>
+    <router-view></router-view>
+    <TypeNav></TypeNav>
+    <ListContainer></ListContainer>
+    <TodayRecommend></TodayRecommend>
+    <Rank></Rank>
+    <Like></Like>
+    <Floor></Floor>
+
+    <Footer v-show="!$route.meta.isHideFooter" ></Footer>
+  </div>
+</template>
+
+<script>
+import Header from "@/components/Header"
+import Footer from "@/components/Footer"
+import Brand from '@/components/Brand'
+import Floor from '@/components/Floor'
+import Like from '@/components/Like'
+import ListContainer from '@/components/ListContainer'
+import Rank from '@/components/Rank'
+import TodayRecommend from '@/components/TodayRecommend'
+import TypeNav from '@/components/TypeNav'
+
+
+export default {
+  name: '',
+  components:{
+    Header,
+    Footer,
+    Brand,
+    Floor,
+    Like,
+    ListContainer,
+    Rank,
+    TodayRecommend,
+    TypeNav
+  }
+}
+</script>
+
+<style lang="less" scoped>
+
+</style>
---
+++ src/router/routes.js
@@ -0,0 +1,33 @@
+import Home from "@/pages/Home"
+import Login from "@/pages/Login"
+import Register from "@/pages/Register"
+import Search from "@/pages/Search"
+
+export default [{
+    path: '/',
+    component: Home,
+  },
+  {
+    path: '/login',
+    component: Login,
+    meta: {
+      isHideFooter: true
+    }
+  },
+  {
+    path: '/register',
+    component: Register,
+    meta: {
+      isHideFooter: true
+    }
+  },
+  {
+    name: 'Search',
+    path: '/search/:keyword?',
+    component: Search,
+    props: route => ({
+      keyword3: route.params.keyword,
+      keyword4: route.query.keyword2
+    })
+  }
+]
